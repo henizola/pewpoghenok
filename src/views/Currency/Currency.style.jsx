3d452381a1428e5196949675560ae2451d790582
Henok Zelalem <z.w.henok@gmail.com>
2022-07-24 17:22:56 +0300

    minor css fixxed

--- src/views/Currency/Currency.style.jsx
+++ src/views/Currency/Currency.style.jsx
@@ -1,18 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 173px 100px;
-  gap: 80px;
-  .pages {
-    display: flex;
-    gap: 1rem;
-  }
-  @media (max-width: 768px) {
-    .pages {
-      flex-direction: column;
-    }
-    margin: 3rem;
-  }
+	display: flex;
+	flex-direction: column;
+	margin: 173px 100px;
+	gap: 80px;
+
+	.pages {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+		grid-gap: 16px;
+	}
+
+	@media (max-width: 768px) {
+		.pages {
+			display: grid;
+			grid-template-columns: 1fr;
+		}
+		margin: 3rem;
+	}
 `;
